0c48ba731b8163e1322e5e8bb85c28e902f7d3d1
smy2363 <smy2363@naver.com>
2024-06-30 17:37:30 +0900

    123

---
+++ java_basic/src/java0628/bingo.java
@@ -0,0 +1,13 @@
+package java0628;
+
+import java.util.Scanner;
+
+public class bingo {
+
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
