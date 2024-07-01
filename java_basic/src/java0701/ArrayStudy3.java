3eef02c00bf17b9bc63a5af94256530e836584d8
smy2363 <smy2363@naver.com>
2024-07-01 09:10:19 +0900

    0701

---
+++ java_basic/src/java0701/ArrayStudy3.java
@@ -0,0 +1,15 @@
+package java0701;
+
+import java.util.Scanner;
+
+public class ArrayStudy3 {
+	
+	static Scanner sc=new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
