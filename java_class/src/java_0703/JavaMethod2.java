e00058f934836c963ccfa45dd83fafe6ea73f4f3
smy2363 <smy2363@naver.com>
2024-07-03 09:07:06 +0900

    0703

---
+++ java_class/src/java_0703/JavaMethod2.java
@@ -0,0 +1,16 @@
+package java_0703;
+
+import java.util.Scanner;
+
+public class JavaMethod2 {
+	
+	static Scanner sc=new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+}
+
+// 메서드나 함수를 사용하는 이유
