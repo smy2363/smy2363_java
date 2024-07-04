9ea7929772367c22dbab7981fe83f9445b992ac3
smy2363 <smy2363@naver.com>
2024-07-04 17:29:18 +0900

    0704

--- java_inherit/src/homeProduct/House.java
+++ java_inherit/src/homeProduct/House.java
@@ -10,6 +10,11 @@ public class House {
 		this.onOff=false;
 	}
 	
+	House(String brand, int price){
+		this.brand=brand;
+		this.price=price;
+	}
+	
 	void power() {
 		this.onOff=!this.onOff;
 		System.out.println("Àü¿ø : "+this.onOff);
--- java_inherit/src/homeProduct/Tv.java
+++ java_inherit/src/homeProduct/Tv.java
@@ -1,8 +1,23 @@
 package homeProduct;
 
 public class Tv extends House {
+	
+	int channel=10;; // 채널
+	int vol=5; // 소리
 
 	Tv(){ // 생성자 메서드
 		super();
+	}
+	// 매개변수 있는 생성자 메서드
+	Tv(String brand, int price){
+		super(brand, price); // 부모클래스 생성자메서드 호출
+	}
+	
+	void channel1Up() {
+		channel++;
+	}
+	
+	void channel1Down() {
+		channel--;
 	}
 }
