bf879379e1c98c09dda78082b106271d76adafa5
smy2363 <smy2363@naver.com>
2024-07-15 16:26:43 +0900

    0715

--- java_collection/src/java_0715_2/TreeSetStudy.java
+++ java_collection/src/java_0715_2/TreeSetStudy.java
@@ -2,18 +2,47 @@ package java_0715_2;
 
 import java.util.TreeSet;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class TreeSetStudy {
 
 	public static void main(String[] args) {
 		
 		TreeSet<Integer> tree = new TreeSet<>();
-		tree.add(30); tree.add(45); tree.add(20);
+		tree.add(34); tree.add(45); tree.add(20);
 		tree.add(9);  tree.add(40); tree.add(15);
 		tree.add(48); tree.add(2);  tree.add(11);
-		tree.add(39);
+		tree.add(39); tree.add(40);
 		
 		System.out.println(tree);
+		System.out.println(tree.first());
+		System.out.println(tree.last());
+		// 입력값보다 큰 값중에서 가장 가까운값 검색
+		System.out.println(tree.higher(15));
+		System.out.println(tree.lower(19));
 		
+		
+		// 입력값보다 작은 모든 값
+		System.out.println(tree.headSet(35));
+		System.out.println(tree.tailSet(30));
+		
+		// 입력값들의 사이 값을 검색(10이상 39미만)
+		System.out.println(tree.subSet(10, 39));
+	
+		
+		TreeSet<String> word = new TreeSet<>();
+		
+		word.add("이순신"); word.add("문익점");
+		word.add("장보고"); word.add("김유신");
+		word.add("정도전"); word.add("박팽년");
+		word.add("성상문"); word.add("마골피");
+		word.add("나훈아");
+		
+		System.out.println(word.higher("아"));
+		System.out.println(word.headSet("라면"));
+		System.out.println(word.subSet("나","자"));
+		
+		System.out.println(word.subSet("ㄴ", "ㅇ"));
 		
 
 	}
