c4db21b830d3dd0fcfe662c079c996f1ae361879
smy2363 <smy2363@naver.com>
2024-07-17 09:21:02 +0900

    0717

--- java_generic/src/java0717/GenericStudy1.java
+++ java_generic/src/java0717/GenericStudy1.java
@@ -3,8 +3,31 @@ package java0717;
 public class GenericStudy1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 }
+
+class A{
+	int[] arr;
+}
+class B{
+	String[] arr;
+}
+class C{
+	float[] arr;
+}
+
+
+
+/*
+
+	Generic
+	
+	- 클래스 내부의 타입을 지정 하는 방법
+	
+
+
+ 
+*/
