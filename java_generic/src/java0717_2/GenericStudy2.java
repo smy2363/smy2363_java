ed239d3531034b2e5a0cd0d124692dbb2fee59ff
smy2363 <smy2363@naver.com>
2024-07-17 12:48:14 +0900

    0717

--- java_generic/src/java0717_2/GenericStudy2.java
+++ java_generic/src/java0717_2/GenericStudy2.java
@@ -12,7 +12,7 @@ public class GenericStudy2 {
 		
 		book.add( new Book("B1234", "자바완전정복", "김동형") );
 		book.add( new Book("C3241", "뭉크모모", "고양이") );
-		book.add( new Book("G4123", "이상적인책", "김모모") );
+		book.add( new Book("G4123", "이상적인책", "모모") );
 		dvd.add( new DVD("M3D232", "포켓몬스터", "뭉크") );
 		dvd.add( new DVD("M4G421", "지렁이게임만들기", "치즈") );
 		dvd.add( new DVD("M5F312", "웹툰모음집", "뿜여사") );
