cddbea4a3147f66a0034d3beeb8839cdb5f05308
smy2363 <smy2363@naver.com>
2024-07-26 10:44:35 +0900

    0726

--- jsp_study1/src/main/java/study/SendEmail.java
+++ jsp_study1/src/main/java/study/SendEmail.java
@@ -2,6 +2,14 @@ package study;
 
 import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class SendEmail {
 	
 	public static void sendEmail(String fromEmail, String title, String content) {
@@ -17,6 +25,45 @@ public class SendEmail {
 		prop.put("mail.smtp.host","smtp.naver.com"); // smtp 서버명
 		prop.put("mail.smtp.port", "587"); // smtp포트번호
 		prop.put("mail.smtp.starttls.enable", "true"); // tls 사용여부
-		prop.pit("mail.smtp.auth","true"); // smtp서버인증
+		prop.put("mail.smtp.auth","true"); // smtp서버인증
+		
+		// 문의글이 등록 되면 관리자 이메일로 전송되어야한다.
+		// 보내는 사람과 받는 사람이 필요한데
+		// 보내는 사람은 클라이언트이지만 실제클라이언트 메일에서 발송되는게
+		// 아니고 자바에서 발송하는것이기때문에
+		// 관리자가 보내고 받고를 해야한다.
+		
+		Session adminAuth = Session.getInstance(prop,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(adminEmail, password);
+					}
+				}
+				
+				
+				); // 보내는 사람 이메일 로그인 인증
+		
+		try {
+			
+			MimeMessage msg = new MimeMessage(adminAuth);
+			msg.setFrom( new InternetAddress(adminEmail)); //보내는 이메일 주소
+			msg.setRecipients(Message.RecipientType.TO, 
+					InternetAddress.parse(adminEmail)); // 받는 이메일 주소
+			
+			msg.setSubject("문의 글 등록 ["+title+"]-"+fromEmail); //이메일 제목
+			
+			msg.setText( // 이메일 내용
+				"<h3>문의 내용</h3> <b>"+content+
+		"</b> <div> <a href='http://localhost:8080/jsp_study1/study/?part=inquiry'>"+
+		"문의글 확인</a></div>"
+					);
+			
+			Transport.send(msg); // 메일 전송
+			
+		}catch(MessagingException e){
+			e.printStackTrace();
+			System.out.println("이메일 전송 실패");
+		}
+		
 	}
 }
