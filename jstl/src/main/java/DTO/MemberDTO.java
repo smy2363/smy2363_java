126eb06d7546eb7f4d8f6b89bc2f78ce2e278a1f
smy2363 <smy2363@naver.com>
2024-07-30 17:31:38 +0900

    0730

--- jstl/src/main/java/DTO/MemberDTO.java
+++ jstl/src/main/java/DTO/MemberDTO.java
@@ -1,6 +1,57 @@
 package DTO;
 
 public class MemberDTO {
-
+	private int memberId;
+	private String userId;
+	private String userPassword;
+	private String userEmail;
+	private String userName;
+	
+	public MemberDTO() {}
+	public MemberDTO(String userId, String userPassword, String userEmail, String userName) {
+		this.userId = userId;
+		this.userPassword = userPassword;
+		this.userEmail = userEmail;
+		this.userName = userName;
+	}
+	public MemberDTO(int id, String userId, String userPassword, String userEmail, String userName){
+		this(userId, userPassword, userEmail, userName);
+		this.memberId = id;
+		
+	}
+	
+	
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
 }
           
