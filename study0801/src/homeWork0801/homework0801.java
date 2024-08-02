45f74612fc5e7ccb8592893c72314737b3626571
smy2363 <smy2363@naver.com>
2024-08-02 22:28:59 +0900

    0802

--- study0801/src/homeWork0801/homework0801.java
+++ study0801/src/homeWork0801/homework0801.java
@@ -8,7 +8,7 @@ public class homework0801 {
 		
 		/* String 메서드
 		charAt(int index) - 특정 위치의 문자를 리턴/ 리턴타입 char
-		equals(Object anObject) - 두 문자열을 비교 / 리턴타입 boolean
+		equals(Object anObject) - 두 문자열을 비교+대소문자 비교 / 리턴타입 boolean
 		getBytes() - byte[]로 리턴/ 리턴타입 byte[]
 		getBytes(Charset charset) - 주어진 문자셋으로 인코딩한 byte[]로 리턴/리턴타입byte[]
 		indexOf(String str) - 문자열 내에서 주어진 문자열의 위치를 리턴/ 리턴타입 int
@@ -21,7 +21,12 @@ public class homework0801 {
 		toLowerCase() - 알파벳 소문자로 변환한 새로운 문자열을 리턴 / 리턴타입 String
 		toUpperCase() - 알파벳 대문자로 변환한 새로운 문자열을 리턴/ 리턴타입 String
 		trim() - 앞뒤 공백을 제거한 새로운 문자열을 리턴 / 리턴타입 String
-		valueOf(int i), valueOf(double d) - 기본타입 값을 문자열로 리턴합니다. / 리턴타입 String															
+		valueOf(int i), valueOf(double d) - 기본타입 값을 문자열로 리턴합니다. / 리턴타입 String		
+		equalsIgnoreCase(String anotherString) 	- 대소문자 무시하고 문자열 비교
+		split(String regex): 문자열을 지정된 정규 표현식을 기준으로 분할합니다.
+		contains(CharSequence s): 특정 문자열이 포함되어 있는지 확인합니다.													
+															
+															
 		
 		*/
 		
@@ -30,7 +35,7 @@ public class homework0801 {
 		char c1=subject.charAt(0);
 		char c2=subject.charAt(4);
 		
-		System.out.println(subject.charAt(3));
+		System.out.println(subject.charAt(3));	
 		System.out.println("첫번째 문자는 "+c1);
 		System.out.println("네번째 문자는 "+c2);
 	
@@ -270,12 +275,139 @@ public class homework0801 {
 	        System.out.println("String sr2 :" + st2);
 	        
 	        
-	
+	       //equalsIgnoreCase - 대소문자 무시하고 문자열 비교
 	        
-	    }
-
+	        String nu1="APPLE";
+	        String nu2="apple";
+	        
+	        System.out.println(nu1.equals(nu2)); 
+	        System.out.println(nu1.equalsIgnoreCase(nu2));
+	        
+	        //split - 입력받은 정규표현식 또는 특정문자를 기준으로 문자열을 나누어 배열에 저장하여 리턴한다.
+	        
+	        String ph="010-1234-5678";
+	        String[] moph=ph.split("-");
+	        String ph1=moph[0];
+	        String ph2=moph[1];
+	        String ph3=moph[2];
+	        
+	        System.out.println(ph1);
+	        System.out.println(ph2);
+	        System.out.println(ph3);
+	        
+	        
+	        String pp="사과-바나나-배-수박";
+	        String[] pp1=pp.split("-");
+	        String pp2=pp1[0];
+	        String pp3=pp1[1];
+	        String pp4=pp1[2];
+	        String pp5=pp1[3];
+	        
+	        System.out.println(pp2);
+	        System.out.println(pp3);
+	        System.out.println(pp4);
+	        System.out.println(pp5);
+	        
+	        String aa="34567-12312-231-23214";
+	        String[] ab=aa.split("-");
+	        String ab1=ab[0];
+	        String ab2=ab[1];
+	        String ab3=ab[2];
+	        String ab4=ab[3];
+	        
+	        System.out.println(ab1);
+	        System.out.println(ab2);
+	        System.out.println(ab3);
+	        System.out.println(ab4);
+	        
+	        String qw="사과-바나나-오리";
+	        String[] qw1=qw.split("-");
+	        String qw2=qw1[0];
+	        String qw3=qw1[1];
+	        String qw4=qw1[2];
+	        
+	        System.out.println(qw2);
+	        System.out.println(qw3);
+	        System.out.println(qw4);
+	        
+	        //contain - 특정문자열이 포함되어있는지 확인
+	        
+	        String er="my life is good";
+	        
+	        System.out.println(er.contains("my"));
+	        System.out.println(er.contains(" life"));
+	        System.out.println(er.contains(" my"));
+	        
+	        //lastIndexOf - 문자열끝에서부터 몇번째 위치하는지 인덱스로 리턴
+	        
+	        String ra="abcd e f g";
+	        
+	        System.out.println(ra.lastIndexOf("e"));
+	        
+	        //isEmpty() - 문자열의 길이가 0인경우 true를 리턴함
+	        
+//	        String ss1=null;
+//	        String ss2="";
+//	        String ss3=" ";
+//	        
+//	        System.out.println(isStringEmpty(ss1));
+//	        System.out.println(isStringEmpty(ss2));
+//	        System.out.println(isStringEmpty(ss3));
+	        
+	        
+	        //StartsWith- 문자열이 지정된 접두사로 시작하는지 확인
+	        
+	        String yy="banan like love true dodo";
+	        
+	        System.out.println(yy.startsWith("ban"));
+	        System.out.println(yy.startsWith("bna",2));
+	        System.out.println(yy.startsWith("nan",2));
+	        
+	        System.out.println(yy.endsWith("odo"));
+	        
+	        //concat - 문자열 합치기
+	        String q="concat과";
+	        String w="append의 차이";
+	        System.out.println(q.concat(w));
+	        
+	        //append - 2개의 문자열이상 합치기
+	        
+	        StringBuffer qwe=new StringBuffer("사과");
+	        System.out.println(qwe);
+	        
+	        System.out.println(qwe.append("맛있다"));
+	        System.out.println(qwe.append("너무나"));
+	        System.out.println(qwe.append("또먹고싶다"));
+	        
+	        //contentEquals - str변수의 문자열과 매개변수의 문자열값이 일치하면 true를 리턴함(내용만비교함)
+	        
+	        String aaa="abc";
+	        String bbb="abc";
+	        StringBuffer ccc= new StringBuffer("abc");
+	        
+	        System.out.println(aaa.equals(bbb));
+	        System.out.println(aaa.equals(ccc));//equals는 인스턴스인지 확인하기에 false가뜸
+	        
+	        System.out.println(aaa.contentEquals(bbb));
+	        System.out.println(aaa.contentEquals(ccc)); //내용만 확인하기에 true가 뜸
+	        
+	        //intern() 문자열비교할때 사용 (연산자 사용안하고 String pool에등록됨_
+	        
+	        String s1=new String("hello");
+	        String s2="hello";
+	        String s3=s1.intern();
+	        System.out.println(s1==s2); //타입이달라서 false /레퍼런스가 다르다
+	        System.out.println(s2==s3);	//타입이 같아서 true / 레퍼런스가 같다 
+	        		
+	        
 
 	}
+	
+//	static boolean isStringEmpty(String ss) {
+//		return ss==null||ss.isEmpty();
+	}
+
+
 
 	
 
